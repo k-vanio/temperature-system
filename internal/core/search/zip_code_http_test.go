94811643a7bb21b88b6b5a960266425dd219df11
Vanio <almeida.vanio@pm.me>
2024-01-19 21:51:34 -0300

    feat: add test with error in viacep

--- internal/core/search/zip_code_http_test.go
+++ internal/core/search/zip_code_http_test.go
@@ -1,7 +1,10 @@
 package search_test
 
 import (
+	"errors"
+	"io"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/k-vanio/temperature-system/internal/config"
@@ -35,5 +38,75 @@ func TestZipCodeHttp(t *testing.T) {
 		// Assert
 		assert.Equal(t, http.StatusUnprocessableEntity, response.Status)
 		assert.EqualValues(t, struct{ Message string }{Message: "invalid zipCode"}, response.Body)
+
+		clientStub.AssertExpectations(t)
+	})
+
+	t.Run("should return error when zipCode is not found", func(t *testing.T) {
+		// Arrange
+		clientStub := &zipCodeHttpStub{}
+		config := config.New()
+
+		zcr := search.New(clientStub, config)
+
+		clientStub.On("Do", mock.Anything).Return(&http.Response{}, errors.New("can not found zipCode"))
+
+		// Act
+		req := dto.SearchRequest{ZipCode: "12345678"}
+		response := zcr.Search(req)
+
+		// Assert
+		assert.Equal(t, http.StatusNotFound, response.Status)
+		assert.EqualValues(t, struct{ Message string }{Message: "can not found zipCode"}, response.Body)
+
+		clientStub.AssertExpectations(t)
+	})
+
+	t.Run("should return error when body is nil", func(t *testing.T) {
+		// Arrange
+		clientStub := &zipCodeHttpStub{}
+		config := config.New()
+
+		zcr := search.New(clientStub, config)
+
+		clientStub.On("Do", mock.Anything).Return(&http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader("{-}")),
+		}, nil)
+
+		// Act
+		req := dto.SearchRequest{ZipCode: "12345678"}
+		response := zcr.Search(req)
+
+		// Assert
+		assert.Equal(t, http.StatusNotFound, response.Status)
+		assert.EqualValues(t, struct{ Message string }{Message: "can not found zipCode"}, response.Body)
+
+		clientStub.AssertExpectations(t)
+	})
+
+	t.Run("should return error when body not localidade", func(t *testing.T) {
+		// Arrange
+		clientStub := &zipCodeHttpStub{}
+		config := config.New()
+
+		zcr := search.New(clientStub, config)
+
+		requestMatch, _ := http.NewRequest(http.MethodGet, "https://viacep.com.br/ws/12345-678/json/", nil)
+
+		clientStub.On("Do", requestMatch).Return(&http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(strings.NewReader(`{"zipCode": "12345678"}`)),
+		}, nil)
+
+		// Act
+		req := dto.SearchRequest{ZipCode: "12345678"}
+		response := zcr.Search(req)
+
+		// Assert
+		assert.Equal(t, http.StatusNotFound, response.Status)
+		assert.EqualValues(t, struct{ Message string }{Message: "can not found zipCode"}, response.Body)
+
+		clientStub.AssertExpectations(t)
 	})
 }
